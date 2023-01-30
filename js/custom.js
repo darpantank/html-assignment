f47195c682b340033626a8ad09f2618fca601f81
darpantank <darpantank2001@gmail.com>
2023-01-30 23:09:08 +0530

    Assignment 3

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
 function openSidebar() {
     if(document.getElementById("mySidebar").style.display == "block"){
         document.getElementById("mySidebar").style.display = "none";
-        document.getElementById("mainContent").style = "margin-left:2%";
+        document.getElementById("mainContent").style = "margin-left:0%";
     }
     else{
         document.getElementById("mySidebar").style.display = "block";
-        document.getElementById("mainContent").style = "margin-left:20%";
+        document.getElementById("mainContent").style = "margin-left:18%";
     }
 }
